a3a52ba6888c593fdbe34b7ed19a55ec958b6ee0
 Anastasia <anastaasiaan@gmail.com>
2023-09-04 23:39:46 +0300

    Find max

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// a = 5; b = 7 -> max = 7;
+// a = 2 b = 10 -> max = 10;
+// a = -9 b = -3 -> max = -3;
+
+using System.ComponentModel.DataAnnotations;
+
+int a = -9;
+int b = -3;
+
+if (a > b)
+{
+    Console.Write ("Max = ");
+    Console.WriteLine (a);
+}
+else 
+{
+    Console.Write ("Max = ");
+    Console.WriteLine (b);
+}
